295e20d79e5e77ffed2d5c6e29819045f572be6b
EritonSoares <ee_soares@hotmail.com>
2025-01-06 17:22:00 -0300

    Migração do tema Modernize

---
+++ src/router/MainRoutes.js
@@ -0,0 +1,14 @@
+export default {
+    path: '/',
+    component: () => import('@/layouts/full/FullLayout.vue'),
+    
+    children: [
+        {
+            name: 'Dashboard',
+            path: '/',
+            component: () => import('@/views/dashboard/index.vue')
+        },
+    ]
+        
+};
+
