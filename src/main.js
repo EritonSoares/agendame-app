a99178f1388966125d9f8a70ff33d17a0390c70e
EritonSoares <ee_soares@hotmail.com>
2025-01-07 11:33:48 -0300

    Aula 16

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
+import App from "./App.vue";
+import router from "./router";
+import { createApp } from "vue";
+import vuetify from "./plugins/vuetify";
+import "@/scss/style.scss";
 
-import App from './App.vue'
-import router from './router'
-import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
-import '@/scss/style.scss';
-
-const app = createApp(App)
-app
-.use(router)
-.use(vuetify)
-.mount('#app')
+const app = createApp(App);
+app.use(router).use(vuetify).mount("#app");
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,36 +1,51 @@
 <script setup lang="ts">
-import Logo from '../../layouts/full/logo/Logo.vue';
-import LoginForm from '../../components/auth/LoginForm.vue';
-import axios from 'axios';
+import Logo from "../../layouts/full/logo/Logo.vue";
+import LoginForm from "../../components/auth/LoginForm.vue";
+import axios from "axios";
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
-axios.get('http://localhost:8000/sanctum/csrf-cookie').then(() => {
-        axios.post('http://localhost:8000/api/login')
-    });
+axios.get("http://localhost:8000/sanctum/csrf-cookie").then(() => {
+  axios.post("http://localhost:8000/api/login", {
+    email: "testw@example.com",
+    password: "password",
+  });
+});
 </script>
 <template>
-    <div class="authentication">
-        <v-container fluid class="pa-3">
-            <v-row class="h-100vh d-flex justify-center align-center">
-                <v-col cols="12" lg="4" xl="3" class="d-flex align-center">
-                    <v-card rounded="md" elevation="10" class="px-sm-1 px-0 withbg mx-auto" max-width="500">
-                        <v-card-item class="pa-sm-8">
-                            <div class="d-flex justify-center py-4">
-                                <Logo />
-                            </div>
-                            <div class="text-body-1 text-muted text-center mb-3">Your Social Campaigns</div>
-                            <LoginForm />
-                            <h6 class="text-h6 text-muted font-weight-medium d-flex justify-center align-center mt-3">
-                                New to Modernize?
-                                <RouterLink :to="{name: 'register'}"
-                                    class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium pl-2">
-                                    Create an account</RouterLink>
-                            </h6>
-                        </v-card-item>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-    </div>
+  <div class="authentication">
+    <v-container fluid class="pa-3">
+      <v-row class="h-100vh d-flex justify-center align-center">
+        <v-col cols="12" lg="4" xl="3" class="d-flex align-center">
+          <v-card
+            rounded="md"
+            elevation="10"
+            class="px-sm-1 px-0 withbg mx-auto"
+            max-width="500"
+          >
+            <v-card-item class="pa-sm-8">
+              <div class="d-flex justify-center py-4">
+                <Logo />
+              </div>
+              <div class="text-body-1 text-muted text-center mb-3">
+                Your Social Campaigns
+              </div>
+              <LoginForm />
+              <h6
+                class="text-h6 text-muted font-weight-medium d-flex justify-center align-center mt-3"
+              >
+                New to Modernize?
+                <RouterLink
+                  :to="{ name: 'register' }"
+                  class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium pl-2"
+                >
+                  Create an account</RouterLink
+                >
+              </h6>
+            </v-card-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
