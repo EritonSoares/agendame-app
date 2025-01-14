7f056e304bd41782608efa92ddfaadd478f90ccb
EritonSoares <ee_soares@hotmail.com>
2025-01-14 10:52:49 -0300

    aula 16

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,16 +1,6 @@
-<script setup lang="ts">
+<script setup>
 import Logo from "../../layouts/full/logo/Logo.vue";
 import LoginForm from "../../components/auth/LoginForm.vue";
-import axios from "axios";
-
-axios.defaults.withCredentials = true;
-axios.defaults.withXSRFToken = true;
-axios.get("http://localhost:8000/sanctum/csrf-cookie").then(() => {
-  axios.post("http://localhost:8000/api/login", {
-    email: "testw@example.com",
-    password: "password",
-  });
-});
 </script>
 <template>
   <div class="authentication">
@@ -27,19 +17,16 @@ axios.get("http://localhost:8000/sanctum/csrf-cookie").then(() => {
               <div class="d-flex justify-center py-4">
                 <Logo />
               </div>
-              <div class="text-body-1 text-muted text-center mb-3">
-                Your Social Campaigns
-              </div>
               <LoginForm />
               <h6
                 class="text-h6 text-muted font-weight-medium d-flex justify-center align-center mt-3"
               >
-                New to Modernize?
+                Nono no AgendaMe?
                 <RouterLink
                   :to="{ name: 'register' }"
                   class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium pl-2"
                 >
-                  Create an account</RouterLink
+                  Criar Conta</RouterLink
                 >
               </h6>
             </v-card-item>
